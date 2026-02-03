29df4ab23e9fce7aa2d1d75af734450ad93de36b
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2026-02-03 20:38:59 +0100

    Remove deploy scripts - use docker compose directly

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Aria Brain - Quick Deploy to Mac
-REM Usage: deploy.bat [commit message]
-
-set MSG=%*
-if "%MSG%"=="" set MSG=Auto-deploy: %date% %time%
-
-powershell -ExecutionPolicy Bypass -File "%~dp0deploy-to-mac.ps1" -Message "%MSG%"
